daa2d3d5b2d7ede0bc965ae3e8db20b787a0e375
wangchen <842724563@qq.com>
2020-05-21 19:15:36 +0800

    好像也不是很麻烦

--- src/main/java/com/wangchen/controller/Test.java
+++ src/main/java/com/wangchen/controller/Test.java
@@ -3,5 +3,6 @@ package com.wangchen.controller;
 public class Test {
     public static void main(String[] args) {
         System.out.println("提交成功了！");
+        System.out.println("又提交成功了！");
     }
 }
